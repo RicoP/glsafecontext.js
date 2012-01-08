221fa35c4fd9d939a13283acf8ead43ddbb7f9e5
Rico P <MailToRico@googlemail.com>
2012-01-08 20:21:46 +0100

    change build process

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+interleave src/utils/constands.main.js -o src/utils/constands.js
+cd src/utils/
+node constands.js > glmethods.js 
+cd ../../
 interleave src/glsavecontext.main.js -o glsavecontext.js
-echo 'compile...' 
+echo 'compile...'
 java -jar closure-compiler/compiler.jar --js glsavecontext.js --js_output_file glsavecontext.min.js
 
